97be78b1437e884f4aa456f8c36282e9e5d111cb
gabox2002 <gabox2002@gmail.com>
2023-04-26 19:23:46 -0300

    comentar header footer creacion de boton ir arriba

--- js/index.js
+++ js/index.js
@@ -4,3 +4,31 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible");
 })
+
+
+
+// Ir arriba
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 5));
+    }
+}
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 600){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 600){
+        buttonUp.style.transform = "scale(0)";
+    }
+}
