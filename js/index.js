963b152742740d3011f0a65b4972d4dc6a758d5f
gabox2002 <gabox2002@gmail.com>
2023-05-13 06:36:05 -0300

    m

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
+////////////////////////////PRODUCTOS////////////////////////////////////
+
+//SELECCIONO TODOS LOS ELEMENTOS DE LA CLASS SLIDER 
+const sliders = document.querySelectorAll('.slider');
+
+sliders.forEach(slider => {
+  const item = slider.querySelector('.item');
+  const point = slider.querySelectorAll('.point');
+
+
+  point.forEach((element, i) => {
+    point[i].addEventListener('click', () => {
+      let position = i;
+      let operation = position * -100;
+      item.style.transform = `translateX(${operation}%)`;
+
+      point.forEach((element, i) => {
+        point[i].classList.remove('active');
+      });
+
+      point[i].classList.add('active');
+    });
+  });
+});
+
+//FILTRADO DE PRODUCTOS
+
+//FUNCION DONDE TOMO LOS ITEMS
+document.addEventListener('DOMContentLoaded', function () {
+    const categoryItems = document.querySelectorAll('.category_item');
+    const productItems = document.querySelectorAll('.product-item');
+
+    categoryItems.forEach(function (item) {
+      item.addEventListener('click', function (event) {
+        event.preventDefault();
+        const category = item.getAttribute('category');
+
+        productItems.forEach(function (product) {
+          if (product.getAttribute('category') === category || category === 'all') {
+            product.style.display = 'block';
+          } else {
+//SINO ES IGUAL OCULTO LOS ITEMS QUE NO TENGAN ESA CATEGOTY
+            product.style.display = 'none';
+          }
+        });
+      });
+    });
+  });
+
+////////////////////////////Fin de PRODUCTOS////////////////////////////////////
+
+
+
 //Mostrar menu desplegable para vistas de smartphone
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
@@ -58,4 +111,7 @@ function showImg(index) {                                                  // Fu
 }
 
 showImg(currentImg);                                                       // Ejecutar la función para mostrar la primera imagen
+
+
+
 
