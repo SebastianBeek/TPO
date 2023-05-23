7676fd73c8fddd6fe988c82714c95867a4cc3b77
gabox2002 <gabox2002@gmail.com>
2023-05-23 10:52:38 -0300

    ajustes de archivos js

--- js/index.js
+++ js/index.js
@@ -1,56 +1,3 @@
-////////////////////////////PRODUCTOS////////////////////////////////////
-
-//SELECCIONO TODOS LOS ELEMENTOS DE LA CLASS SLIDER 
-const sliders = document.querySelectorAll('.slider');
-
-sliders.forEach(slider => {
-  const item = slider.querySelector('.item');
-  const point = slider.querySelectorAll('.point');
-
-
-  point.forEach((element, i) => {
-    point[i].addEventListener('click', () => {
-      let position = i;
-      let operation = position * -100;
-      item.style.transform = `translateX(${operation}%)`;
-
-      point.forEach((element, i) => {
-        point[i].classList.remove('active');
-      });
-
-      point[i].classList.add('active');
-    });
-  });
-});
-
-//FILTRADO DE PRODUCTOS
-
-//FUNCION DONDE TOMO LOS ITEMS
-document.addEventListener('DOMContentLoaded', function () {
-    const categoryItems = document.querySelectorAll('.category_item');
-    const productItems = document.querySelectorAll('.product-item');
-
-    categoryItems.forEach(function (item) {
-      item.addEventListener('click', function (event) {
-        event.preventDefault();
-        const category = item.getAttribute('category');
-
-        productItems.forEach(function (product) {
-          if (product.getAttribute('category') === category || category === 'all') {
-            product.style.display = 'block';
-          } else {
-//SINO ES IGUAL OCULTO LOS ITEMS QUE NO TENGAN ESA CATEGOTY
-            product.style.display = 'none';
-          }
-        });
-      });
-    });
-  });
-
-////////////////////////////Fin de PRODUCTOS////////////////////////////////////
-
-
-
 //Mostrar menu desplegable para vistas de smartphone
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
@@ -82,15 +29,3 @@ window.onscroll = function(){
 
 /***************************************************************/
 
-/*api de google*/
-function iniciarMap(){
-  var coord = {lat:-34.612230882972426 ,lng: -58.385131716449784};
-  var map = new google.maps.Map(document.getElementById('map'),{
-    zoom: 10,
-    center: coord
-  });
-  var marker = new google.maps.Marker({
-    position: coord,
-    map: map
-  });
-}
