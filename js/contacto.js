06f2112c4d79159fcf39cad9c01018ad23a74ce7
SebastianBeek <127237189+SebastianBeek@users.noreply.github.com>
2023-05-25 13:21:30 -0300

    Update contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -45,24 +45,22 @@ function validacion(){
   
   
   
-  
-  const $form = document.querySelector('#form')
+const $form = document.querySelector('#myForm')
 
-  $form.addEventListener('submit', handleSubmit)
+$form.addEventListener('submit', handleSubmit)
 
-  async function handleSubmit(event) {
-    event.preventDefault()
-    const form = new FormData(this)
-    const response = await fetch(this.action, {
-      method: this.method,
-      body: form,
-      headers: {
-        'Accept': 'application/json'
+async function handleSubmit(event) {
+      event.preventDefault()
+      const form = new FormData(this)
+      const response = await fetch (this.action, {
+          method: this.method,
+          body: form,
+          headers: {
+              'Accept': 'application/json'
+          }
+      })
+      if (response.ok) {
+          this.reset()
+          alert('Su mensaje ha sido enviado con éxito, espere devolución')
       }
-    })
-    if (response.ok) {
-      this.reset()
-      
-      alert('Su mensaje ha sido enviado con éxito, espere devolución')
-    }
   }
